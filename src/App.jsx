bf54f27a4e312fc217dc5cd043aa5967eaac1ee5
Amil <amilseferov305@gmail.com>
2025-05-18 01:14:12 +0400

    header added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <header className="w-full h-fit bg-gradient-to-r from-red-900 via-yellow-600 to-blue-900  px-[20px] py-[10px]">
+        <div className="flex font-bold text-[26px] ">
+          <p className="text-white text-shadow-blue-700 text-shadow-2xs">Shah</p>
+          <p className="text-amber-300">Food</p>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
